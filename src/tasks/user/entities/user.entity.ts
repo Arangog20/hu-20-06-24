153e4e3546edf4f389af423c1dfc83274fa7af46
Arangog20 <149792894+Arangog20@users.noreply.github.com>
2024-06-21 10:00:49 -0500

    Creation of user entities and their corresponding DTOs

---
+++ src/tasks/user/entities/user.entity.ts
@@ -0,0 +1,20 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  userId:number;
+
+  @Column()
+  name:string;
+
+  @Column()
+  lastName:string;
+
+  @Column()
+  email:string;
+
+  @Column()
+  phone:number;
+
+}
