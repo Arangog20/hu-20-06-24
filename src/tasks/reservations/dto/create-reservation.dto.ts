06751c3fc249a57d69a456f333454b082aebc4d4
Arangog20 <149792894+Arangog20@users.noreply.github.com>
2024-06-21 08:51:14 -0500

    Creation of reservations entities and their corresponding DTOs

--- src/tasks/reservations/dto/create-reservation.dto.ts
+++ src/tasks/reservations/dto/create-reservation.dto.ts
@@ -1,1 +1,32 @@
-export class CreateReservationDto {}
+import { IsBoolean, IsDate, IsEnum, IsNotEmpty, IsNumber, IsOptional, IsPositive } from "class-validator";
+import { Status } from "../entities/reservation.entity";
+
+export class CreateReservationDto {
+  @IsOptional()
+  @IsNumber()
+  @IsPositive()
+  reservationId: number;
+
+  @IsNotEmpty()
+  @IsPositive()
+  @IsNumber()
+  userId: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  @IsPositive()
+  sessionId: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  @IsPositive()
+  workSpaceId: number;
+
+  @IsNotEmpty()
+  @IsDate()
+  reservationDate: Date;
+
+  @IsNotEmpty()
+  @IsEnum(Status)
+  status: Status;
+}
